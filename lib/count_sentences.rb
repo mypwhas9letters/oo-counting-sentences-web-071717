0605fd98c90a0a1702dbd2428bf396bd8ad8aaf7
Hui Wang <huiw3939@gmail.com>
2017-07-19 16:54:41 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+  ends = ['.','?','!']
+    self.split(" ").count do |x|
+    ends.any? do |y|
+      x.end_with?(y)
+      end
+    end
   end
+
+
 end
